94972d46a5cb97c9066dc1fcd519634d36539118
yash-shah17 <88420125+yash-shah17@users.noreply.github.com>
2022-09-03 14:55:18 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React, { Component } from 'react'
+import NavBar from './Components/NavBar';
+import News from './Components/News';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar/>
+        <News/>
+      </div>
+    )
+  }
+}
+
---
+++ src/Components/NewsItem.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+  render() {
+    return (
+        <div>
+            this is a news comp
+             
+        </div>
+        )
+  }
+}
+
+export default NewsItem
